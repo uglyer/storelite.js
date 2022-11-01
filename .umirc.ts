e481f0fbc38b19483c7b6af7b7046f545ef8cac9
uglyer <root@wanda.sb>
2022-11-01 22:21:20 +0800

    🔧 配置支持 wasm 加载

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,30 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  // extraBabelPlugins: process.env.NODE_ENV === 'production' ? ['babel-plugin-dynamic-import-node'] : [],
+  chainWebpack(config) {
+    // config.set('experiments', {
+    //   ...config.get('experiments'),
+    //   asyncWebAssembly: true
+    // })
+
+    const REG = /\.wasm$/;
+
+    config.module.rule('asset').exclude.add(REG).end();
+
+    config.module
+      .rule('wasm')
+      .test(REG)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        name: 'assets/wasm/[hash:16].[ext]',
+        limit: 1,
+      })
+      .end()
+      .exclude.end()
+      .type('javascript/auto')
+      .end();
+  },
   // more config: https://d.umijs.org/config
 });
