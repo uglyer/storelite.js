ab05fc0c7504d1ce9fa95aa2330fc88313d039ea
uglyer <root@wanda.sb>
2022-11-09 23:11:47 +0800

    🚧 初始化动态表单

---
+++ src/store/libs/StoreLiteDynamicFrom.ts
@@ -0,0 +1,33 @@
+import { SqlDB } from '@/declaration/SqlDB';
+
+/**
+ * StoreLiteDynamicFrom
+ * @author uglyer
+ * @date 2022/11/9 22:59
+ */
+export class StoreLiteDynamicFrom {
+  constructor(protected db: SqlDB) {}
+
+  /**
+   * 初始化数据库
+   * 执行建表
+   * @protected
+   */
+  protected initDB() {
+    const createTable = `
+      create table if not exists store_lite_data
+      (
+        id
+        integer
+        not
+        null
+        primary
+        key,
+        table_name
+        char,
+        content
+        json
+      )`;
+    this.db.exec(createTable);
+  }
+}
