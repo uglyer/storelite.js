28cf1ba4396ca796334a879c705f44c18b92b4b3
uglyer <root@wanda.sb>
2022-11-13 21:03:38 +0800

    🚧 实现 Where 条件 基础 DSL

--- src/store/libs/orm/SqlDBWhereConditionBasicImpl.ts
+++ src/store/libs/orm/SqlDBWhereConditionBasicImpl.ts
@@ -1,22 +1,55 @@
 import {
+  BaseType,
   SqlDBBasicWhereCondition,
   SqlDBBasicWhereConditionType,
+  SqlDBInstruction,
 } from '@/declaration/SqlDB';
+import { SqlDBBasicWhereConditionImpl } from '@/store/libs/orm/SqlDBBasicWhereConditionImpl';
 
 /**
  * SqlDB Where 条件 基础 DSL 实现类
  * @author uglyer
  * @date 2022/11/13 0:01
  */
-export class SqlDBWhereConditionBasicImpl<T, C>
+export abstract class SqlDBWhereConditionBasicImpl<T, C>
   implements SqlDBBasicWhereConditionType<T, C>
 {
+  /**
+   * 条件集合
+   * @protected
+   */
+  protected list: Array<['AND' | 'OR', string]> = [];
+
+  /**
+   * 获取C实例
+   * @protected
+   */
+  protected abstract getInstance(): C;
+
+  /**
+   * 添加条件 条件
+   * @param type
+   * @param handler
+   */
+  protected andCondition(
+    type: 'AND' | 'OR',
+    handler: (condition: SqlDBBasicWhereCondition<T>) => void,
+  ): C {
+    const condition = new SqlDBBasicWhereConditionImpl<T>();
+    handler(condition);
+    const sql = condition.toSql(type);
+    if (sql != null) {
+      this.list.push([type, sql]);
+    }
+    return this.getInstance();
+  }
+
   /**
    * and 条件
    * @param handler
    */
   and(handler: (condition: SqlDBBasicWhereCondition<T>) => void): C {
-    return undefined;
+    return this.andCondition('AND', handler);
   }
 
   /**
@@ -24,6 +57,6 @@ export class SqlDBWhereConditionBasicImpl<T, C>
    * @param handler
    */
   or(handler: (condition: SqlDBBasicWhereCondition<T>) => void): C {
-    return undefined;
+    return this.andCondition('OR', handler);
   }
 }
