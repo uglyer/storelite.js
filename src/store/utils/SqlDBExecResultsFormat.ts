267fff959ecf79b719835ad025aa5cfa54f80221
uglyer <root@wanda.sb>
2022-12-07 19:11:42 +0800

    ✨ 新增查询结果转换实体工具类

---
+++ src/store/utils/SqlDBExecResultsFormat.ts
@@ -0,0 +1,37 @@
+import { SqlDBExecResult } from '@/declaration/SqlDB';
+
+/**
+ * sql 执行结果格式化工具
+ * @author uglyer
+ * @date 2022/12/7 19:04
+ */
+export class SqlDBExecResultsFormat {
+  /**
+   * 转换为实体类（多个）
+   * @param result
+   */
+  toEntities<T>(result: SqlDBExecResult): T[] {
+    const list: T[] = [];
+    for (let i = 0; i < result.value.length; i++) {
+      const obj = {} as any;
+      for (let j = 0; j < result.columns.length; j++) {
+        obj[result.columns[j]] = result.value[i][j];
+      }
+      list.push(obj);
+    }
+    return list;
+  }
+
+  /**
+   * 转换为实体类（单个）
+   * @param result
+   * @param index
+   */
+  toEntity<T>(result: SqlDBExecResult, index = 0): T {
+    const obj = {} as any;
+    for (let i = 0; i < result.columns.length; i++) {
+      obj[result.columns[i]] = result.value[index][i];
+    }
+    return obj;
+  }
+}
