b6cb579c0f82c9715ee509c026e4e751ab5ca674
uglyer <root@wanda.sb>
2022-12-10 22:31:58 +0800

    🚧 元数据支持定义获取视图名称

--- src/store/libs/ioc/EntityMetadata.ts
+++ src/store/libs/ioc/EntityMetadata.ts
@@ -18,7 +18,7 @@ class EntityMetadataImpl {
     fieldName: string,
     type: ColumnType,
     jsType: EntityJsTypes,
-  ) {
+  ): void {
     let list = Reflect.getMetadata('storelite:type', object.constructor);
     if (!Array.isArray(list)) {
       list = [];
@@ -41,6 +41,28 @@ class EntityMetadataImpl {
       Reflect.getMetadata('storelite:type', object.constructor) ??
       null
     );
+  }
+
+  /**
+   * 定义视图名称
+   * @param object
+   * @param viewName
+   */
+  defineViewName(object: Object, viewName: string): void {
+    Reflect.defineMetadata('storelite:view-name', viewName, object);
+  }
+
+  /**
+   * 获取视图名称（未定义类型将抛出异常）
+   * @param object
+   */
+  getViewName(object: Object): string {
+    const viewName = Reflect.getMetadata('storelite:view-name', object);
+    if (!viewName) {
+      console.error('未定义视图名称', object);
+      throw Error('未定义视图名称');
+    }
+    return viewName;
   }
 }
 
