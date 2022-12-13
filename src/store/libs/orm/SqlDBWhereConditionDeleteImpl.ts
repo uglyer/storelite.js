ab0205d8e7d19c03b623f45b168876fa4b0c2985
unily <root@wanda.sb>
2022-12-13 20:24:47 +0800

    🚧 实现删除语句构建并执行

--- src/store/libs/orm/SqlDBWhereConditionDeleteImpl.ts
+++ src/store/libs/orm/SqlDBWhereConditionDeleteImpl.ts
@@ -1,18 +1,41 @@
 import { SqlDBWhereConditionBasicImpl } from '@/store/libs/orm/SqlDBWhereConditionBasicImpl';
+import { SqlDB, SqlDBWhereConditionDelete } from '@/declaration/SqlDB';
 
 /**
- * SqlDBWhereConditionDeleteImpl
+ * SqlDB Where 条件 删除 DSL 实现类
  * @author uglyer
  * @date 2022/12/12 20:28
  */
-export class SqlDBWhereConditionDeleteImpl<
-  T,
-> extends SqlDBWhereConditionBasicImpl<T, SqlDBWhereConditionDeleteImpl<T>> {
+export class SqlDBWhereConditionDeleteImpl<T>
+  extends SqlDBWhereConditionBasicImpl<T, SqlDBWhereConditionDeleteImpl<T>>
+  implements SqlDBWhereConditionDelete<T>
+{
+  constructor(
+    protected db: SqlDB,
+    protected entity: T,
+    protected tableName: string,
+  ) {
+    super();
+  }
+
   /**
    * 获取C实例
    * @protected
    */
   protected getInstance(): SqlDBWhereConditionDeleteImpl<T> {
     return this;
+  }
+
+  /**
+   * 执行删除语句
+   */
+  do(): number {
+    let sql = this.toSql();
+    if (sql == null) {
+      throw Error('删除指令必须添加条件');
+    }
+    sql = `DELETE FROM ${this.tableName} WHERE ${sql}`;
+    this.db.exec(sql);
+    return -1;
   }
 }
